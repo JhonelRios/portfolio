9a8fc29bbb6144a3487851f5e89edf4bc15825ab
Jhonel Rios Jaimes <riosjaimes.j@gmail.com>
2022-08-23 23:06:43 -0500

    feat: social links icons

--- components/ProfileCard.jsx
+++ components/ProfileCard.jsx
@@ -1,5 +1,7 @@
 import styles from '../styles/components/profile-card.module.css';
 
+import { SiLinkedin, SiGithub } from 'react-icons/si';
+
 export default function ProfileCard() {
   return (
     <article className={styles.card}>
@@ -16,6 +18,24 @@ export default function ProfileCard() {
           web development and improve my knowledge into topics such as SEO, accessibility
           and web performance.
         </p>
+
+        <footer className={styles.footer}>
+          <a
+            href="https://www.linkedin.com/in/jhonelrios/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiLinkedin />
+          </a>
+
+          <a
+            href="https://github.com/JhonelRios"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiGithub />
+          </a>
+        </footer>
       </div>
     </article>
   );
